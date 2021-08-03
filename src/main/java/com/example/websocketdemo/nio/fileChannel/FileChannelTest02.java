df2406968e404bfaa5d76c2e5573bf3dbe72f70c
LLLLLL <913577706@qq.com>
2021-08-03 17:39:49 +0800

    filechannel测试

--- src/main/java/com/example/websocketdemo/nio/fileChannel/FileChannelTest02.java
+++ src/main/java/com/example/websocketdemo/nio/fileChannel/FileChannelTest02.java
@@ -6,25 +6,27 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Collections;
 
 public class FileChannelTest02 {
 
     public static void main(String[] args) {
-        ByteBuffer buffer = ByteBuffer.allocate(80);
+        ByteBuffer buffer = ByteBuffer.allocate(9);
         FileInputStream fileInputStream = null;
         try {
             fileInputStream = new FileInputStream("E:\\aa.txt");
             FileChannel fileChannel = fileInputStream.getChannel();
             try {
-                fileChannel.read(buffer);
+                while (true) {
+                    buffer.clear();
+                    int read = fileChannel.read(buffer);
+                    System.out.println("read=="+read);
+                    if (read == -1) {
+                        break;
+                    }
+                    System.out.println(new String(buffer.array()));
+                }
             } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                String str = new String(buffer.array());
-                System.out.println(str);
-
-            } catch (Exception e) {
                 e.printStackTrace();
             }
         } catch (FileNotFoundException e) {
--- src/main/java/com/example/websocketdemo/nio/fileChannel/FileChannelTest03.java
+++ src/main/java/com/example/websocketdemo/nio/fileChannel/FileChannelTest03.java
@@ -1,4 +1,23 @@
 package com.example.websocketdemo.nio.fileChannel;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class FileChannelTest03 {
+
+    public static void main(String[] args) throws Exception {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        FileInputStream fileInputStream = new FileInputStream("1.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream("2.txt");
+        FileChannel fileChannel = fileInputStream.getChannel();
+        fileChannel.read(byteBuffer);
+        byteBuffer.flip();
+        FileChannel outFileChannel = fileOutputStream.getChannel();
+        outFileChannel.write(byteBuffer);
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
 }
