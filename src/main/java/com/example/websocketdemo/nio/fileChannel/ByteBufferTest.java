3b6ad5e596511140a28303217b4e3754b5627942
LLLLLL <913577706@qq.com>
2021-08-04 13:25:48 +0800

    filechannel测试

--- src/main/java/com/example/websocketdemo/nio/fileChannel/ByteBufferTest.java
+++ src/main/java/com/example/websocketdemo/nio/fileChannel/ByteBufferTest.java
@@ -5,11 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
+/**
+ * 使用MappedByteBuffer直接对文件在内存中修改(堆外内存)
+ */
 @Slf4j
 public class ByteBufferTest {
 
     public static void main(String[] args) {
-        String str = "asdnskfksaljda1222231";
+        /*String str = "asdnskfksaljda1222231";
         ByteBuffer buffer = ByteBuffer.allocate(30);
        // for (int i = 0; i < buffer.capacity(); i++) {
             buffer.put(str.getBytes());
@@ -20,7 +23,21 @@ public class ByteBufferTest {
 
             System.out.println(buffer.get());
 
-        }
+        }*/
 
+        readOnlyBuffer();
+    }
+
+    public static String readOnlyBuffer() {
+        ByteBuffer buffer = ByteBuffer.allocate(32);
+        for (int i = 0; i < 7; i++) {
+            buffer.putInt(i);
+        }
+        buffer.flip();
+        ByteBuffer readOnlyBuffer = buffer.asReadOnlyBuffer();
+        while (readOnlyBuffer.hasRemaining()) {
+            System.out.println(readOnlyBuffer.getInt());
+        }
+        return "success";
     }
 }
