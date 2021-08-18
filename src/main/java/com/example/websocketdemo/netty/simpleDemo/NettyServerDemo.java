882343a405765f59cc68d0f19404158cfb334398
LLLLLL <913577706@qq.com>
2021-08-18 17:36:33 +0800

    netty入门2

--- src/main/java/com/example/websocketdemo/netty/simpleDemo/NettyServerDemo.java
+++ src/main/java/com/example/websocketdemo/netty/simpleDemo/NettyServerDemo.java
@@ -1,8 +1,12 @@
 package com.example.websocketdemo.netty.simpleDemo;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
@@ -11,20 +15,46 @@ import io.netty.channel.socket.nio.NioSocketChannel;
  */
 public class NettyServerDemo {
 
-    public static void main(String[] args) {
-        //创建BossGroup和 WorkGroup
-        //bossGroup只处理连接请求，客户端业务处理是由workGroup处理
-        //
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+    public static void main(String[] args) throws Exception{
+        NioEventLoopGroup bossGroup = null;
+        NioEventLoopGroup workGroup = null;
+        try{
+            //创建BossGroup和 WorkGroup
+            //bossGroup只处理连接请求，客户端业务处理是由workGroup处理
+            //
+            bossGroup = new NioEventLoopGroup();
+            workGroup = new NioEventLoopGroup();
 
-        //创建服务器端启动对象，设置参数
-        ServerBootstrap bootstrap = new ServerBootstrap();
-    /*    bootstrap.group(bossGroup,workGroup)//设置线程组
-                .channel(NioServerSocketChannel.class)//使用NioServerSocketChannel做为服务器通道实现
-                .option(ChannelOption.SO_BACKLOG, 128)//设置线程队列等待连接个数
-                .childOption(ChannelOption.SO_KEEPALIVE, true)//设置保持连接状态
-                .childHandler(new)//创建一个通道初始化对象 匿名对象*/
+            //创建服务器端启动对象，设置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            //设置线程组
+            bootstrap.group(bossGroup,workGroup)
+                    //使用NioServerSocketChannel做为服务器通道实现
+                    .channel(NioServerSocketChannel.class)
+                    //设置线程队列等待连接个数
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    //设置保持连接状态
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    //创建一个通道初始化对象 匿名对象
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            //为pipeline管道添加处理器
+                            socketChannel.pipeline().addLast(new NettyServerHandler());
+                        }
+
+                    });
+
+            //绑定端口号并同步,并生成一个ChannelFuture对象
+            ChannelFuture channelFuture = bootstrap.bind(8888).sync();
+
+            //对关闭的通道进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
 
     }
 
