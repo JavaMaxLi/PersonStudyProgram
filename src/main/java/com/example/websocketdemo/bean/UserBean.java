46c25af225e6e7d71a0438d5115df52dc20e3f47
LLLLLL <913577706@qq.com>
2021-08-24 10:31:29 +0800

    基于netty搭建的HTTP服务器

---
+++ src/main/java/com/example/websocketdemo/bean/UserBean.java
@@ -0,0 +1,46 @@
+package com.example.websocketdemo.bean;
+
+import java.io.Serializable;
+
+public class UserBean implements Serializable {
+
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public UserBean setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public UserBean setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public UserBean setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "UserBean{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
