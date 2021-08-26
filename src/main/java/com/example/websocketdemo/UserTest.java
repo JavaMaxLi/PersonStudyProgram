e7105b86e91abadb00104fba20ced65490e552c0
LLLLLL <913577706@qq.com>
2021-08-26 10:10:12 +0800

    UsetTest

--- src/main/java/com/example/websocketdemo/UserTest.java
+++ src/main/java/com/example/websocketdemo/UserTest.java
@@ -3,6 +3,8 @@ package com.example.websocketdemo;
 import com.example.websocketdemo.proxy.ComputerFactory;
 import com.example.websocketdemo.proxy.DynamicProxyFactory;
 import com.example.websocketdemo.proxy.ProxyFactory;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.util.NettyRuntime;
 
 import java.lang.reflect.InvocationHandler;
@@ -10,6 +12,27 @@ import java.lang.reflect.Proxy;
 
 public class UserTest {
     public static void main(String[] args) {
+        byteBufTest();
+
+    }
+
+
+    public static void byteBufTest() {
+
+        ByteBuf buf = Unpooled.buffer(10);
+
+        for (int i = 0; i < 10; i++) {
+            buf.writeByte(i);
+        }
+
+        System.out.println("buf的大小为："+buf.capacity());
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(buf.readByte());
+        }
+    }
+
+    public static void proxy() {
         ProxyFactory computer = new ComputerFactory();
 
         DynamicProxyFactory dynamicProxyFactory = new DynamicProxyFactory(computer);
@@ -19,6 +42,5 @@ public class UserTest {
         ProxyFactory proxy = (ProxyFactory) Proxy.newProxyInstance(computer.getClass().getClassLoader(),computer.getClass().getInterfaces(),dynamicProxyFactory);
 
         proxy.method();
-
     }
 }
